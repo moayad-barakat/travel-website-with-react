78f18283e73003480511884b95fa5acea6829ff6
moayad-barakat <155306707+moayad-barakat@users.noreply.github.com>
2025-03-11 22:07:41 +0300

    Update App.jsx

--- travel-agency/travel-agency/src/App.jsx
+++ travel-agency/travel-agency/src/App.jsx
@@ -13,6 +13,7 @@ function App() {
     <Router>
       <Navbar/>
       <Routes>
+       <Route path="" element={<Home/>}/>
         <Route path="/home" element={<Home/>}/>
         <Route path ="/about" element ={<About/>}/>
         <Route path="/loading" element ={<Loadding/>}/>
